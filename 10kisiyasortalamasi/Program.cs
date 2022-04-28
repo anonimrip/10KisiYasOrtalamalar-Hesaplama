24e820d31225938ee7472f40ef67c29779364ee6
EREN FELEKOGLU <EREN FELEKOGLU@BIL-12>
2022-04-28 12:11:25 +0300

    Proje dosyası ekle.

---
+++ 10kisiyasortalamasi/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace nothesaplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int[] yazili_notlari = new int[10];
+            int topla = 0;
+            for (int i = 0; i < yazili_notlari.Length; i++)
+            {
+                Console.WriteLine(i+". Yaşını Giriniz: ");
+                yazili_notlari[i] = Convert.ToInt32(Console.ReadLine());
+                topla = topla + yazili_notlari[i];
+
+            }
+            Console.WriteLine("Toplamları: " + topla);
+            int hesapla = topla / yazili_notlari.Length;
+            Console.Write("Yaş Ortalamaları: ");
+            Console.Write(hesapla);
+        }
+    }
+}
